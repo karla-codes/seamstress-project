445a6b925f50f409431edded1570e3fc365570ef
karla-walker <indiablonix@gmail.com>
2020-08-21 06:18:05 -0500

    Added functionality to second slideshow

--- js/slides.js
+++ js/slides.js
@@ -1,24 +1,27 @@
 // selects slide show container
-const slides = document.querySelector("section div.slides");
+const slides = document.querySelectorAll("section div.slides");
 
-let currentImg = 0;
-let z = 1000000;
+slides.forEach(slide => {
+  let currentImg = 0;
+  let z = 1000000;
+  
+  // selects images and adds them to a list
+  const images = slide.querySelectorAll("img");
+  console.log(images);
+  
+  // sets the z-index for each image
+  images.forEach(image => {
+    z--;
+    image.style.zIndex = z;
+  });
+  
+  // slide function
+  // each click decreases the z-index of current image and sends it to the bottom
+  slide.addEventListener("click", function () {
+    z--;
+    images[currentImg].style.zIndex = z;
+    currentImg++;
+    currentImg = currentImg % images.length;
+  });
+})
 
-// selects images and adds them to a list
-const images = document.querySelectorAll("img");
-console.log(images);
-
-// sets the z-index for each image
-images.forEach(image => {
-  z--;
-  image.style.zIndex = z;
-});
-
-// slide function
-// each click decreases the z-index of current image and sends it to the bottom
-slides.addEventListener("click", function () {
-  z--;
-  images[currentImg].style.zIndex = z;
-  currentImg++;
-  currentImg = currentImg % images.length;
-});
